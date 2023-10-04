f7ddf870030b89e8b95804676545d9a93d1af70c
Mārtiņš Sliede <martinssliede@gmail.com>
2023-10-04 19:13:24 +0300

    Restructuring code

--- script.js
+++ script.js
@@ -1,67 +1,158 @@
 import jsonList from './example.json' assert { type: 'json'};
 
-// Get elements
 //Galvenā izvēlne;
-const izvēlne = document.getElementById('itemsSelect');
+const mainDropdown = document.getElementById('itemsSelect');
 // Koda teksts;
-const precesKods = document.getElementById('selectedItem');
+const itemCodeTitle = document.getElementById('selectedItem');
 // Konteiners, kurā parādīsies papildu izvēlnes;
-const konteiners = document.getElementById('varietiesContainer');
+const varietyDropdownContainer = document.getElementById('varietiesContainer');
+const selectedDropdownValues = {};
 
-
-function updateSelectedItemCode() {
-    const izvēlētaisIndekss = izvēlne.selectedIndex;
-    if (izvēlētaisIndekss !== 0) {
-        const izvēlētaisKods = izvēlne.value;
-        const datiIzvēlētajāKodā = jsonList.items.find(item => item.code === izvēlētaisKods);
-        if (datiIzvēlētajāKodā) {
-
-            // Notīra esošās papildu izvēlnes;
-            konteiners.innerHTML = '';
-
-            if (datiIzvēlētajāKodā.varieties.length > 0) {
-                datiIzvēlētajāKodā.varieties.forEach(varietyCode => {
-
-                  // Izveido papildu izvēlnes
-                  const varietyDropdown = document.createElement('select');
-                  varietyDropdown.id = varietyCode;
-                  varietyDropdown.name = varietyCode;
-                  varietyDropdown.appendChild(new Option(varietyCode));
-                  
-                  varietyDropdown.addEventListener('change', function() {
-                    const selectedValue = this.value;
-                
-
-                    // PRECES KODA TEKSTS
-                  precesKods.textContent = "Preces kods: " + datiIzvēlētajāKodā.code + "." + selectedValue + ".";
-                  });
-    
-                  // Piepilda ar JSON datiem
-                  const varietyData = jsonList.varieties.find(variety => variety.code === varietyCode);
-                  varietyData.options.forEach(option => {
-                    varietyDropdown.appendChild(new Option(option.description, option.code));
-                  });
-
-                  konteiners.appendChild(varietyDropdown);
-                });
-            } else {
-                precesKods.textContent = "Preces kods: " + datiIzvēlētajāKodā.code;
-            }
-        }
-  
-    } else {
-        precesKods.textContent = "Izvēlies produktu no saraksta!";
-    }
+function clearVarietyDropdowns() {
+    // Notīra esošās papildu izvēlnes;
+    varietyDropdownContainer.innerHTML = '';
 }
 
+// Izveido papildus izvēlnes
+function createVarietyDropdown(varietyCode, varietyData, selectedItemData) {
+    const varietyDropdown = document.createElement('select');
+    varietyDropdown.id = varietyCode;
+    varietyDropdown.name = varietyCode;
+    varietyDropdown.appendChild(new Option(varietyCode));
+  
+    varietyData.options.forEach(option => {
+      varietyDropdown.appendChild(new Option(option.description, option.code));
+    });
+  
+    varietyDropdown.addEventListener('change', function() {
+      selectedDropdownValues[varietyCode] = this.value;
+      updateCodeText(selectedItemData);
+    });
+  
+    varietyDropdownContainer.appendChild(varietyDropdown);
+    selectedDropdownValues[varietyCode] = "";
+  }
 
-// Filling item select dropdown;
-jsonList.items.forEach(item => {
-const option = document.createElement('option');
-option.value = item.code;
-option.text = item.description;
-izvēlne.appendChild(option);
-});
+    // Piepilda mainDropdown ar select opcijām
+  function populateMainDropdown() {
+    jsonList.items.forEach(item => {
+      const option = document.createElement('option');
+      option.value = item.code;
+      option.text = item.description;
+      mainDropdown.appendChild(option);
+    });
+  }
 
-// Add an event listener to the select element
-izvēlne.addEventListener('change', updateSelectedItemCode);
+    // Uzslēdz event listener uz katrām izmaiņām;
+  function initialize() {
+    mainDropdown.addEventListener('change', updateSelectedItemCode);
+    mainDropdown.addEventListener('change', updateCodeText);
+    populateMainDropdown();
+  }
+
+  // Pārbauda vai izvēlas visas vērtības un atgriež kodu, ja tam nav papildu izvēlnes;
+  function updateSelectedItemCode() {
+    const selectedIndex = mainDropdown.selectedIndex;
+    if (selectedIndex !== 0) {
+      const selectedItemCode = mainDropdown.value;
+      const selectedItemData = jsonList.items.find(item => item.code === selectedItemCode);
+      if (selectedItemData) {
+        clearVarietyDropdowns();
+        if (selectedItemData.varieties.length > 0) {
+          selectedItemData.varieties.forEach(varietyCode => {
+            const varietyData = jsonList.varieties.find(variety => variety.code === varietyCode);
+            createVarietyDropdown(varietyCode, varietyData, selectedItemData);
+          });
+        } else {
+          itemCodeTitle.textContent = "Preces kods: " + selectedItemData.code;
+        }
+      }
+    } else {
+      itemCodeTitle.textContent = "Izvēlies produktu no saraksta!";
+    }
+  }
+  
+  // Ģenerē preces kodu atkarībā no precei pieejamajām opcijām;
+  function updateCodeText(selectedItem) {
+    const selectedVarietyValues = Object.values(selectedDropdownValues);
+    const allValuesSelected = selectedVarietyValues.every(value => value !== '');
+    
+    if (allValuesSelected) {
+      const codeText = `${selectedItem.code}.${selectedVarietyValues.join('.')}`;
+      itemCodeTitle.textContent = "Preces kods: " + codeText;
+    } else {
+      itemCodeTitle.textContent = "Lūdzu, izvēlieties visas vērtības!";
+    }
+}
+  
+  initialize();
+
+
+// NESAKĀRTOTS
+// function updateSelectedItemCode() {
+//     const selectedIndex = mainDropdown.selectedIndex;
+//     if (selectedIndex !== 0) {
+//         const selectedItemCode = mainDropdown.value;
+//         const selectedItemData = jsonList.items.find(item => item.code === selectedItemCode);
+//         if (selectedItemData) {
+//             mainDropdown.addEventListener('change', updateSelectedItemCode);
+            
+//             // Notīra esošās papildu izvēlnes;
+//             varietyDropdownContainer.innerHTML = '';
+
+//             if (selectedItemData.varieties.length > 0) {
+//                 selectedItemData.varieties.forEach(varietyCode => {
+
+//                   // Izveido papildus izvēlnes
+//                   const varietyDropdown = document.createElement('select');
+//                   varietyDropdown.id = varietyCode;
+//                   varietyDropdown.name = varietyCode;
+//                   varietyDropdown.appendChild(new Option(varietyCode));
+    
+//                   // Piepilda ar JSON datiem
+//                   const varietyData = jsonList.varieties.find(variety => variety.code === varietyCode);
+//                   varietyData.options.forEach(option => {
+//                     varietyDropdown.appendChild(new Option(option.description, option.code));
+//                   });
+
+//                   varietyDropdown.addEventListener('change', function() {
+//                     selectedDropdownValues[varietyCode] = this.value;
+//                     updateCodeText(selectedItemData);
+//                 });
+
+//                   varietyDropdownContainer.appendChild(varietyDropdown);
+//                   selectedDropdownValues[varietyCode] = "";
+//                 });
+//             } else {
+//                 itemCodeTitle.textContent = "Preces kods: " + selectedItemData.code;
+//             }
+//         }
+//     } else {
+//         itemCodeTitle.textContent = "Izvēlies produktu no saraksta!";
+//     }
+// }
+
+// // PRECES KODA TEKSTS
+// function updateCodeText(selectedItem) {
+//     const selectedVarietyValues = Object.values(selectedDropdownValues);
+//     const allValuesSelected = selectedVarietyValues.every(value => value !== '');
+    
+//     if (allValuesSelected) {
+//         const codeText = `${selectedItem.code}.${selectedVarietyValues.join('.')}`;
+//         itemCodeTitle.textContent = "Preces kods: " + codeText;
+//     } else {
+//         itemCodeTitle.textContent = "Lūdzu, izvēlieties visas vērtības!";
+//     }
+// }
+
+// // Filling mainDropdown;
+// jsonList.items.forEach(item => {
+// const option = document.createElement('option');
+// option.value = item.code;
+// option.text = item.description;
+// mainDropdown.appendChild(option);
+// });
+
+// // Add an event listener to the select element
+// mainDropdown.addEventListener('change', updateSelectedItemCode);
+// mainDropdown.addEventListener("change", updateCodeText);
